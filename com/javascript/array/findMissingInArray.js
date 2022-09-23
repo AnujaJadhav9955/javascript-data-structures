f4ac65a1e59bc9ed0e33c6f2e66458f1ef81473b
AnujaJadhav <GS-3019@GSLAB.COM>
2022-09-23 23:29:22 +0530

    find missing element from an array

---
+++ com/javascript/array/findMissingInArray.js
@@ -0,0 +1,12 @@
+function findMissingInArray(arr, n) {
+let sum = 0;
+for (let i=0;i<arr.length; i++) {
+    sum = sum + arr[i];
+}
+const sumN = n*(n+1)/2;
+return sumN-sum;
+}
+
+const arr = [3, 2, 1, 5]
+const missingElement = findMissingInArray(arr, 5);
+console.log('missingElement', missingElement);
