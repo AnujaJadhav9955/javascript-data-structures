a34a86da909c35ca8c50bddc28335fa340f329e2
AnujaJadhav <GS-3019@GSLAB.COM>
2022-09-23 23:30:34 +0530

    find missing element from an array

--- com/javascript/array/findMissingInArray.js
+++ com/javascript/array/findMissingInArray.js
@@ -3,7 +3,7 @@ let sum = 0;
 for (let i=0;i<arr.length; i++) {
     sum = sum + arr[i];
 }
-const sumN = n*(n+1)/2;
+const sumN = n*(n+1)/2; // sum of n number from 1-n => n*(n+1)/2
 return sumN-sum;
 }
 
